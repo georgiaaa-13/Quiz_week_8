aacfe35bbd81a122b31db84ced3899a71553bc9b
Georgia <georgia.platt@griffithuni.edu.au>
2023-09-15 15:20:05 +1000

    Completed Q1 and Q2

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,29 @@
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+import sqlite3
+
+connection = sqlite3.connect(r'climate.db')
+cursor = connection.cursor()
+
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+data = cursor.fetchall()
+
+years = [row[0] for row in data]
+co2 = [row[1] for row in data]
+temp = [row[2] for row in data]
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.xlabel("Year (decade)")
+
+plt.subplots_adjust(hspace=0.3)
 
 plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.savefig("co2_temp_1.png")
+
+connection.close()
--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+df = pd.read_csv(r"climate.csv")
+
+years = df['Year']
+co2 = df['CO2']
+temp = df['Temperature']
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
+
+plt.subplots_adjust(hspace=0.3)
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
-
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.savefig("co2_temp_2.png")
+plt.show()
